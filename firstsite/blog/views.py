828d71d3d1c31cf8f1756fa9a43b55ec074d05d5
Presniu <goncharovvv11@gmail.com>
2022-05-23 21:25:12 +0300

    Protect secret key

--- firstsite/blog/views.py
+++ firstsite/blog/views.py
@@ -9,7 +9,10 @@ class Home(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
 
-
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     content = super().get_context_data(**kwargs)
+    #     extra = self.get_post_content(title='Main page')
+    #     return dict(list(content.items()) + list(extra.items()))
 
 def showpost(request):
     return HttpResponse('Post')
